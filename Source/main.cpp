0487c2c23942f7119f74c41c629d8c6f7362a4fb
adityajedhe <adityajedhe@gmail.com>
2025-01-31 01:20:43 +0530

    conv

--- Source/main.cpp
+++ Source/main.cpp
@@ -10,8 +10,8 @@
 #include "DiskStack.h"
 
 
-// 
-// --------------------------------------------
+// Global variables to count the number of transfers
+// -------------------------------------------------
 int nbIterations = 0;
 
 /**
