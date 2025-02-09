27ce594329b22a639805753b06690b1159b5aa2d
adityajedhe <adityajedhe@gmail.com>
2025-02-10 00:21:44 +0530

    Update main.cpp header: add description and author information

--- Binary_Tree_Operations/Sources/main.cpp
+++ Binary_Tree_Operations/Sources/main.cpp
@@ -1,5 +1,9 @@
 //===================================================================
-// Name: main.cpp
+// Name         : main.cpp
+// Description  : This file contains the main function that lists
+// all the operations that can be performed on a binary tree.
+// @author      : Aditya Jedhe [@adityajedhe]
+// @date        : 2025/02/10
 //===================================================================
 
 #include <iostream>
