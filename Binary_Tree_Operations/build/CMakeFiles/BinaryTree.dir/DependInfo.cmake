8dcf2c660612fbbbc47e42e6b85ff25d9f4caaaf
Aditya Jedhe <adityajedhe@gmail.com>
2025-08-01 22:36:08 +0530

    cmake changes

---
+++ Binary_Tree_Operations/build/CMakeFiles/BinaryTree.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/adity/Documents/Developments/Project_BinaryTree/Binary_Tree_Operations/Sources/BinaryTree.cpp" "CMakeFiles/BinaryTree.dir/Sources/BinaryTree.cpp.obj" "gcc" "CMakeFiles/BinaryTree.dir/Sources/BinaryTree.cpp.obj.d"
+  "C:/Users/adity/Documents/Developments/Project_BinaryTree/Binary_Tree_Operations/Sources/main.cpp" "CMakeFiles/BinaryTree.dir/Sources/main.cpp.obj" "gcc" "CMakeFiles/BinaryTree.dir/Sources/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Common/build/CMakeFiles/NodeLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NodeLibrary.dir/Sources/Node.cpp.obj"
+  "CMakeFiles/NodeLibrary.dir/Sources/Node.cpp.obj.d"
+  "libNodeLibrary.dll"
+  "libNodeLibrary.dll.a"
+  "libNodeLibrary.dll.manifest"
+  "libNodeLibrary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NodeLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
